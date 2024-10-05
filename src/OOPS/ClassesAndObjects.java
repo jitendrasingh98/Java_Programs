0a79ec8200567ee0ed1d4f757ad50f87a64a8abb
Jitendra Singh <118965215+jitendrasingh98@users.noreply.github.com>
2024-10-05 15:13:57 +0530

    add constructor note file and update oop note

--- src/OOPS/ClassesAndObjects.java
+++ src/OOPS/ClassesAndObjects.java
@@ -1,7 +1,7 @@
 package OOPS;
-public class First {
+public class ClassesAndObjects{
     public static void main(String[] args) {
-        Student std = new Student();
+        Student std = new Student();//creating object
         System.out.println(std.roolno);
         System.out.println(std.name);
         System.out.println(std.marks);
